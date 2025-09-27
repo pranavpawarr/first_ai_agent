4ffe3cfb53b3b1a28b07f4e435dbf85df39bc952
pranavpawarr <pranavpawar503@gmail.com>
2025-09-27 20:06:13 +0530

    user can now give input using cli

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from first-ai-agent!")
+import sys
+import os
+from dotenv import load_dotenv
+from google import genai
 
+
+def main():
+    if len(sys.argv) < 2:
+        print("Error: Please provide a prompt as a command line argument.")
+        sys.exit(1)
+    
+    prompt = sys.argv[1]
+
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=prompt)
+    
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
